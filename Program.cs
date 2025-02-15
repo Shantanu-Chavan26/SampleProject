e3bff0d96bf34d2a031fa2ff6a411538a6f681ca
Shantanu <shantanuchavan2603@gmail.com>
2025-02-15 15:18:51 +0530

    Removed exclamation mark

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace SimpleConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World");
             Console.WriteLine("This is a simple C# console application.");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
