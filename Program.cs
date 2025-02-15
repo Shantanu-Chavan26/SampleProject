c0beb418ac144726c266732a1aceff9f2bb1d689
Shantanu <shantanuchavan2603@gmail.com>
2025-02-15 15:12:04 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a simple C# console application.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
